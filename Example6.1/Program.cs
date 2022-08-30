07900f04b130a5fdbbe8e339f470c3f6d2504f2e
Berus Frager <gitmailru@yandex.ru>
2022-08-30 12:19:28 +0300

    Коммит файлов папки проекта Example6.1

---
+++ Example6.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Получение строковых пар
+using System.Linq;  // Подключение системной библиотеки
+string text = "(1,2) (2,3) (4,5) (6,7)";
+var data = text.Split(" ") // Неявный тип data. У строки есть
+                            // метод Split делает разбивку
+                            // с учетом символа разделителя
+                .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
